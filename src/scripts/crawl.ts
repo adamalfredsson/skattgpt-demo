c559806b36cad5c0588c77b1170b4ad2b54efa47
Adam Alfredsson <alfredsson.adam@gmail.com>
2023-09-07 15:31:10 +0200

    Enqueue links

--- src/scripts/crawl.ts
+++ src/scripts/crawl.ts
@@ -1,5 +1,22 @@
-const crawl = () => {
-  console.log("crawl");
+import { CheerioCrawler, createCheerioRouter } from "crawlee";
+
+const URL = "https://www.skatteverket.se";
+
+const router = createCheerioRouter();
+
+router.addDefaultHandler(async ({ enqueueLinks, request }) => {
+  console.log(request.url);
+
+  await enqueueLinks({});
+});
+
+const crawl = async () => {
+  const crawler = new CheerioCrawler({
+    requestHandler: router,
+    maxRequestsPerCrawl: 5,
+  });
+
+  await crawler.run([URL]);
 };
 
 crawl();
