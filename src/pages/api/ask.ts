adaf891fb3f0cd5a73e1510c53b9ea67e7ad5327
Adam Alfredsson <alfredsson.adam@gmail.com>
2023-09-07 14:58:57 +0200

    Add openai

--- src/pages/api/ask.ts
+++ src/pages/api/ask.ts
@@ -1,15 +1,39 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import OpenAI from "openai";
 import { type Message } from "..";
 
-export default function handler(
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+async function ask(question: string, history: Message[]) {
+  const res = await openai.chat.completions.create({
+    messages: [
+      ...history,
+      {
+        role: "user",
+        content: question,
+      },
+    ],
+    model: "gpt-3.5-turbo",
+  });
+  return res.choices[0].message;
+}
+
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Message>
 ) {
   const { question } = req.body;
+  const answer = await ask(question, []);
+
+  if (!answer.content) {
+    throw new Error("No answer from OpenAI");
+  }
 
   res.json({
-    content: "Hi there!" + question,
+    content: answer.content,
     role: "assistant",
   });
 }
