990f5f0f668302a4fe46b370fafc2dbfeb5b9976
Adam Alfredsson <alfredsson.adam@gmail.com>
2023-09-08 12:00:15 +0200

    OpenAI functions

--- src/pages/api/ask.ts
+++ src/pages/api/ask.ts
@@ -3,58 +3,96 @@ import dedent from "dedent";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { openai } from "../../services/openai";
 import { COLLECTION_NAME, qdrant } from "../../services/qdrant";
-import { Message } from "../../types";
+import { Document, Message } from "../../types";
 
-async function ask(question: string, history: Message[]) {
-  const questionVector = await openai.embeddings.create({
-    input: [question],
+async function search(query: string) {
+  const queryVector = await openai.embeddings.create({
+    input: [query],
     model: "text-embedding-ada-002",
   });
 
   const documents = await qdrant.search(COLLECTION_NAME, {
-    vector: questionVector.data[0].embedding,
+    vector: queryVector.data[0].embedding,
     limit: 4,
   });
 
+  return documents.map(
+    (document) =>
+      ({
+        id: document.id as string,
+        title: document.payload!.title,
+        content: document.payload!.content,
+        url: document.payload!.url,
+      } as Document)
+  );
+}
+
+async function ask(
+  question: string,
+  history: Message[],
+  documents: Document[] = []
+) {
   const res = await openai.chat.completions.create({
     messages: [
       {
         role: "system",
         content:
-          "Du är en expert som svarar på skattefrågor. Använd alltid information från källorna för att svara, svara annars att du inte vet.",
+          "Du är en expert som svarar på skattefrågor. Du måste söka efter information från skatteverket för att svara på skattefrågor, svara annars att du inte vet.",
       },
       ...history,
       {
         role: "user",
-        content: dedent`
-          Använd följande källor för att svara på frågan:
-          
-          ${documents
-            .map(
-              (document) => dedent`
-                ---
-                ${document.payload!.title}
-
-                ${document.payload!.content}
-                ---
-              `
-            )
-            .join("\n")}
-
-          Fråga: "${question}"
-        `,
+        content: question,
+      },
+      ...(documents.length
+        ? [
+            {
+              role: "function",
+              name: "lookup",
+              content: documents
+                .map(
+                  (document) => dedent`
+                    ---
+                    ${document.title}
+                    ${document.content}
+                    ---
+                  `
+                )
+                .join("\n"),
+            } as const,
+          ]
+        : []),
+    ],
+    functions: [
+      {
+        name: "lookup",
+        description: "Sök efter information på Skatteverkets webbplats",
+        parameters: {
+          type: "object",
+          properties: {
+            query: {
+              type: "string",
+              description: "Sökfråga",
+            },
+          },
+          required: ["query"],
+        },
       },
     ],
     model: "gpt-3.5-turbo",
   });
+
+  const message = res.choices[0].message;
+
+  if (message.function_call?.name === "lookup") {
+    console.log(message.function_call.arguments);
+    const { query } = JSON.parse(message.function_call.arguments);
+    return ask(question, history, [...documents, ...(await search(query))]);
+  }
+
   return {
     message: res.choices[0].message,
-    documents: documents.map((document) => ({
-      id: document.id as string,
-      title: document.payload!.title,
-      content: document.payload!.content,
-      url: document.payload!.url,
-    })),
+    documents,
   };
 }
 
